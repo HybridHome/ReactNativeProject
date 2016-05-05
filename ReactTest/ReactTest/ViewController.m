92112eccf8733319f38bff66e2b9387896b6b1f0
lichuanjun <lichuanjun@niwodai.net>
2016-05-05 13:17:50 +0800

    新增部分功能

--- ReactTest/ReactTest/ViewController.m
+++ ReactTest/ReactTest/ViewController.m
@@ -20,7 +20,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSURL *jsCodeLocation;
-    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+//    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/testjs/04/4.2/index.ios.bundle?platform=ios&dev=true"];
     RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                         moduleName:@"ReactTest"
                                                  initialProperties:nil
@@ -28,13 +29,13 @@
     rootView.frame = [[UIScreen mainScreen]bounds];
     [self.view addSubview:rootView];
     
-    UIButton *searchBtn = [[UIButton alloc]init];
-    searchBtn.frame = CGRectMake(20, 50, 100, 20);
-    searchBtn.backgroundColor = [UIColor colorWithRed:0.000 green:0.569 blue:1.000 alpha:1];
-    [searchBtn setTitle:@"搜索" forState:UIControlStateNormal];
-    //    [searchBtn setTitle:@"搜索" forState:UIControlStateHighlighted];
-    [searchBtn addTarget:self action:@selector(searchTap) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:searchBtn];
+//    UIButton *searchBtn = [[UIButton alloc]init];
+//    searchBtn.frame = CGRectMake(20, 50, 100, 20);
+//    searchBtn.backgroundColor = [UIColor colorWithRed:0.000 green:0.569 blue:1.000 alpha:1];
+//    [searchBtn setTitle:@"搜索" forState:UIControlStateNormal];
+//    //    [searchBtn setTitle:@"搜索" forState:UIControlStateHighlighted];
+//    [searchBtn addTarget:self action:@selector(searchTap) forControlEvents:UIControlEventTouchUpInside];
+//    [self.view addSubview:searchBtn];
 }
 
 - (void)didReceiveMemoryWarning {
